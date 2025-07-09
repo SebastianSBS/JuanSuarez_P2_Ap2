fca668c0ea9440b37d968c67736bbf51aaa85c8a
SebastianSBS <sebas.suarez177@gmail.com>
2025-07-09 19:32:50 -0400

    Creacion de varias carpetas y archivos: Resource, ViajeApi y ViajeDto. Se realizo el archivo ViajeDto.

---
+++ app/src/main/java/com/example/juansuarez_p2_ap2/data/remote/Resource.kt
@@ -0,0 +1,2 @@
+package com.example.juansuarez_p2_ap2.data.remote
+
---
+++ app/src/main/java/com/example/juansuarez_p2_ap2/data/remote/dto/ViajeDto.kt
@@ -0,0 +1,16 @@
+package com.example.juansuarez_p2_ap2.data.remote.dto
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Viaje")
+data class ViajeDto (
+    @PrimaryKey
+    val idViaje: Int? = 0,
+    val fecha: String? = null,
+    val millas: Int? = null,
+    val tasaDolar: Int? = null,
+    val observaciones: String = "",
+    val monto: Double = 0.0
+)
+
